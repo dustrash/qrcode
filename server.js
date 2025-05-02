b99c2620ca18c24626beacc35e0eeb0ece888998
cistori <cas00000000@gmail.com>
2025-05-03 00:41:31 +0900

    port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
